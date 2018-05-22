c8b22bb5a45df27e9a80ed63c04c3f9ccaa6f341
Bobby Towers <porkostomus@mail.com>
2018-05-22 10:13:05 -0700

    Create 52.clj

---
+++ 52.clj
@@ -0,0 +1,3 @@
+{:_id 52 :title "Intro to Destructuring" :tests ["(= [2 4] (let [[a b c d e f g] (range)] __))"], :description "Let bindings and function parameter lists support destructuring.", :tags ["easy" "destructuring"]}
+
+[c e]
