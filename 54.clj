99a4f872aa4c4895ec0d8d8b445fa0a129528fc5
Bobby Towers <porkostomus@mail.com>
2018-05-22 15:45:22 -0700

    Update and rename 54 to 54.clj

--- 54.clj
+++ 54.clj
@@ -1,6 +1,14 @@
 ;; 54 Partition a Seq
 
-Returns seq of lists of x items each, no less
+:restricted "partition" "partition-all"
+:title "Partition a Sequence"
+
+(__ 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))
+(= (__ 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))
+(= (__ 3 (range 8)) '((0 1 2) (3 4 5)))
+
+Write a function which returns a sequence of lists of x items each.
+Lists of less than x items should not be returned.
 
 user=> (__ 3 (range 9))
 ((0 1 2) (3 4 5) (6 7 8))
@@ -9,6 +17,9 @@ user=> (__ 3 (range 9))
  
 (= (__ 3 (range 8)) '((0 1 2) (3 4 5)))
 
+(fn f [n l]
+  (when (>= (count l) n)
+    (cons (take n l)(f n (drop n l)))))
 
 (fn [n coll] (loop [c coll partitioned []] (if (< (count c) n) partitioned 
 (recur (drop n c) (conj partitioned (take n c))))))
