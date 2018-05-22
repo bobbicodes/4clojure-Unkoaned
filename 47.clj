8eb7e6cd120b4823f7236080b7117841ae69d313
Bobby Towers <porkostomus@mail.com>
2018-05-22 09:52:41 -0700

    Update and rename 47 to 47.clj

--- 47.clj
+++ 47.clj
@@ -1,8 +1,15 @@
-;; 47 contain that thang
+;; 47 Contain Yourself
+; The contains? function checks if a KEY is present in a given collection.
+; This often leads beginner clojurians to use it incorrectly with numerically indexed collections like vectors and lists.
 
 user=> (contains? #{4 5 6} 4)
 true
 user=> (contains? [1 1 1 1 1] 4)
+true
+
+user=> (contains? {4 :a 2 :b} 4)
+true
+user=> (not (contains? [1 2 4] 4))
 true
 
 user=> (source contains?)
