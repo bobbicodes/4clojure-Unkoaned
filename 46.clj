3223492a06a6ba1ba5c16eed3549cdf556e846b3
Bobby Towers <porkostomus@mail.com>
2018-05-22 09:35:45 -0700

    Update 46.clj

--- 46.clj
+++ 46.clj
@@ -1,4 +1,5 @@
-;; 46 Flip order of args
+;; 46 Flipping Out
+; "Write a higher-order function which flips the order of the arguments of an input function."
 
 (fn [f] #(f %2 %))
 
@@ -10,6 +11,14 @@ user=> ((flipper nth) 2 [1 2 3 4 5])
 
 user=> ((flipper >) 7 8)
 true
+
+user=> ((flipper quot) 2 8)
+4
+
+user=> ((flipper take) [1 2 3 4 5] 3)
+(1 2 3)
+
+; this one will take more than 2 args
 
 user=> (((fn [f]
   (fn [& args]
