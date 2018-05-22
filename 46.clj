3b50843acfb677b2677be71c0a564eae0db44991
Bobby Towers <porkostomus@mail.com>
2018-05-22 08:00:57 -0700

    Update and rename 46 to 46.clj

--- 46.clj
+++ 46.clj
@@ -1,11 +1,23 @@
 ;; 46 Flip order of args
 
-(((fn [f]
+(fn [f] #(f %2 %))
+
+(defn flipper [f]
+  #(f %2 %))
+
+user=> ((flipper nth) 2 [1 2 3 4 5])
+3
+
+user=> ((flipper >) 7 8)
+true
+
+user=> (((fn [f]
   (fn [& args]
     (apply f
       (reverse args)))) nth) 2 [1 2 3 4 5])
+3
 
-(((fn [f]
+user=> (((fn [f]
     (fn [& args]
       (apply f (reverse args)))) nth) 2 [1 2 3 4 5])
 3
