636a1286cf0c2670de721e43822c05ee78280c77
Bobby Towers <porkostomus@mail.com>
2018-05-22 10:10:22 -0700

    Update and rename 51 to 51.clj

---
+++ 51.clj
@@ -0,0 +1,8 @@
+;; 51 Advanced Destructuring
+; Here is an example of some more sophisticated destructuring
+
+(= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] __] [a b c d]))
+
+(let [[a b & c :as d] (range 1 6)]
+     [a b c d])
+;;=> [1 2 (3 4 5) (1 2 3 4 5)]
