1b774872dbc277291131ee1b4341d3b2c5ce8c7f
Bobby Towers <porkostomus@mail.com>
2018-05-23 04:02:24 -0700

    Create 126.clj

---
+++ 126.clj
@@ -0,0 +1,14 @@
+Through the Looking Class
+Difficulty:	Easy
+Topics:	fun brain-teaser
+
+
+Enter a value which satisfies the following:
+
+(let [x __]
+  (and (= (class x) x) x))
+
+
+Code which fills in the blank:
+
+Class
