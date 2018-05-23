68f7d366d76d2c978c2246892f3090d8905f121a
Bobby Towers <porkostomus@mail.com>
2018-05-22 22:30:10 -0700

    Update and rename 90 to 90.clj

--- 90.clj
+++ 90.clj
@@ -6,6 +6,8 @@
      ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
      ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]}
 
+#(set (for [a % b %2] [a b]))
+
 ((fn [s1 s2] (set (for [a s1 b s2] [a b]))) #{1 2 3} #{4 5})
 ;;=> #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]}
 
