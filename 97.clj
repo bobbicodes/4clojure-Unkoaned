c73a05a5dd26b54b957876bbac9c721a86ef0488
Bobby Towers <porkostomus@mail.com>
2018-05-22 23:38:54 -0700

    Update and rename 97 to 97.clj

--- 97.clj
+++ 97.clj
@@ -6,6 +6,11 @@ adjacent numbers in the row above,
 and adding a 1 to the beginning and end of the row.
 Returns nth row of Pascal's Triangle. 
 	
+#(loop [n 1
+        l [1]]
+   (if (= n %) l
+       (recur (inc n)(vec (map + (cons 0 l)(conj l 0))))))
+	
 ((fn [n] (letfn [(pt [] (iterate (fn [r] (map #(apply + %) (partition 2 1 (concat [0] r [0])))) [1]))] (->> (pt) (take n) (last)))) 1) ;;=> [1]
 
 (map (fn [n] (letfn [(pt [] (iterate (fn [r] (map #(apply + %) (partition 2 1 (concat [0] r [0])))) [1]))] (->> (pt) (take n) (last)))) (range 1 6)) ;;=>
