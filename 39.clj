f596a63ccbf7ab6e3b26d3aec4cf291c2855bb72
Bobby Towers <porkostomus@mail.com>
2018-05-23 12:19:51 -0700

    Update and rename 39 to 39.clj

--- 39.clj
+++ 39.clj
@@ -1,5 +1,11 @@
-interleave - takes two seqs, returns
-first of each, second of each, third, etc.
+{:_id 39 :restricted ["interleave"], :title "Interleave Two Seqs"
+:tests ["(= (__ [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))"
+"(= (__ [1 2] [3 4 5 6]) '(1 3 2 4))"
+"(= (__ [1 2 3 4] [5]) [1 5])"
+"(= (__ [30 20] [25 15]) [30 25 20 15])"]
+:description "Write a function which takes two sequences and returns the first item from each,
+then the second item from each, then the third, etc."
+:tags ["easy" "seqs" "core-functions"]}
 
 user=> (source mapcat)
 (defn mapcat
