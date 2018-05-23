6dded16d25bfc4f382ec9046982f82a5dbc4d5ed
Bobby Towers <porkostomus@mail.com>
2018-05-23 09:25:31 -0700

    Create 132.clj

---
+++ 132.clj
@@ -0,0 +1,28 @@
+Insert between two items
+Difficulty:	Medium
+Topics:	seqs core-functions
+
+Write a function that takes a two-argument predicate, a value, and a collection;
+and returns a new collection where the value is inserted between every two items that satisfy the predicate.
+
+(= '(1 :less 6 :less 7 4 3) (__ < :less [1 6 7 4 3]))
+
+(= '(2) (__ > :more [2]))
+
+(= [0 1 :x 2 :x 3 :x 4]  (__ #(and (pos? %) (< % %2)) :x (range 5)))
+
+(empty? (__ > :more ()))
+
+(= [0 1 :same 1 2 3 :same 5 8 13 :same 21]
+   (take 12 (->> [0 1]
+                 (iterate (fn [[a b]] [b (+ a b)]))
+                 (map first) ; fibonacci numbers
+                 (__ (fn [a b] ; both even or both odd
+                       (= (mod a 2) (mod b 2)))
+                     :same))))
+#(apply concat
+        (take 1 %3)
+        (for [[a b] (partition 2 1 %3)]
+          (if (% a b)
+            [%2 b]
+            [b])))                    
