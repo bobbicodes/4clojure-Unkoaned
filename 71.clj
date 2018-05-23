61d9c84a04e72daf6912241dd962ee91d9014e5a
Bobby Towers <porkostomus@mail.com>
2018-05-22 18:08:32 -0700

    Update and rename 71 to 71.clj

--- 71.clj
+++ 71.clj
@@ -1,12 +1,12 @@
 ;; 71 Rearranging Code: ->
 
--> threads expression x through a variable number of forms.
-x is inserted as second item in first form,
-making a list of it if it is not a list already.
-Then the first form is inserted as second item in second form,
-making a list of that form if necessary.
-This process continues for all the forms.
-Makes code more readable.
+{:_id 71 :title "Rearranging Code: ->"
+:tests ["(= (__ (sort (rest (reverse [2 5 4 1 3 6]))))\n   (-> [2 5 4 1 3 6] reverse rest sort __)\n   5)"]
+:description "The -> macro threads an expression x through a variable number of forms.
+First, x is inserted as the second item in the first form, making a list of it if it is not a list already.
+Then the first form is inserted as the second item in the second form, making a list of that form if necessary.
+This process continues for all the forms.  Using -> can sometimes make your code more readable."
+:tags ["elementary"]}
 
 user=> (source ->)
 (defmacro ->
