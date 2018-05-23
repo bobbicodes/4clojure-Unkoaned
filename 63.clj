dda71703e9a6907b1f1ad2d9226cb484da2a58a7
Bobby Towers <porkostomus@mail.com>
2018-05-22 17:02:54 -0700

    Update and rename 63 to 63.clj

--- 63.clj
+++ 63.clj
@@ -1,5 +1,16 @@
 ;; 63 Group a Seq
 
+{:_id 63 :restricted ["group-by"]
+:title "Group a Sequence"
+:tests [
+"(= (__ #(> % 5) #{1 3 6 8}) {false [1 3], true [6 8]})"
+"(= (__ #(apply / %) [[1 2] [2 4] [4 6] [3 6]])\n   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})"
+"(= (__ count [[1] [1 2] [3] [1 2 3] [2 3]])\n   {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})"]
+:description "Given a function f and a sequence s, write a function which returns a map.
+The keys should be the values of f applied to each item in s.
+The value at each key should be a vector of corresponding items in the order they appear in s."
+:tags ["medium" "seqs" "core-functions"]}
+
 Takes func and seq, returns map
 keys are vals of f applied to each item in s.
 val at each key is a vec of corresponding items
