8dc93c2016223721adf456918d96bbab9bf0012c
Bobby Towers <porkostomus@mail.com>
2018-05-22 19:29:41 -0700

    Update and rename 81 to 81.clj

--- 81.clj
+++ 81.clj
@@ -1,8 +1,14 @@
 ;; 81 Set Intersection
 
-Returns intersection of two sets -
-the sub-set of items that each set has in common.
-Works like intersection
+{:_id 81 :restricted ["intersection"]
+:title "Set Intersection"
+:tests [
+"(= (__ #{0 1 2 3} #{2 3 4 5}) #{2 3})"
+"(= (__ #{0 1 2} #{3 4 5}) #{})"
+"(= (__ #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d})"]
+:description "Write a function which returns the intersection of two sets.
+The intersection is the sub-set of items that each set has in common."
+:tags ["easy" "set-theory"]}
 
 user=> (find-doc " intersection")
 -------------------------
