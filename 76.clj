d55c96164e84dd94f5f8f38b9e3866750fc1f96a
Bobby Towers <porkostomus@mail.com>
2018-05-22 18:47:57 -0700

    Update 76.clj

--- 76.clj
+++ 76.clj
@@ -1,26 +1,4 @@
-;; 76 Trampoline [m]
-
-Takes a function f and a variable number of parameters.
-Calls f with any parameters that were supplied.
-If f returns a function, trampoline calls
-that function with no arguments.
-This is repeated, until the return value is not a function,
-and then trampoline returns that non-function value.
-This is useful for implementing mutually recursive algorithms
-in a way that won't consume the stack.
-
-(= __
-   (letfn
-     [(foo [x y] #(bar (conj x y) y))
-      (bar [x y] (if (> (last x) 10)
-                   x
-                   #(foo x (+ 2 y))))]
-     (trampoline foo [] 1)))
-[1 3 5 7 9 11]
-
-(filter odd? (range 12))
-
-(range 1 12 2)
+{:_id 76 :title "Intro to Trampoline"
 
 (letfn
      [(foo [x y] #(bar (conj x y) y))
@@ -28,6 +6,13 @@ in a way that won't consume the stack.
                    x
                    #(foo x (+ 2 y))))]
      (trampoline foo [] 1))
+ 
+ :description "The trampoline function takes a function f and a variable number of parameters.
+ Trampoline calls f with any parameters that were supplied.
+ If f returns a function, trampoline calls that function with no arguments.
+ This is repeated, until the return value is not a function, and then trampoline returns that non-function value.
+ This is useful for implementing mutually recursive algorithms in a way that won't consume the stack."
+ :tags ["medium" "recursion"]}
 
 ; Here is what is happening:
 ; foo is called with [] 1, returns #(bar [1] 1)
