3edac02ffe6876bfaec401baa6671a6d519baa23
Bobby Towers <porkostomus@mail.com>
2018-05-22 22:17:11 -0700

    Update 88.clj

--- 88.clj
+++ 88.clj
@@ -1,6 +1,8 @@
 ;; 88 Symmetric Difference-
 the set of items belonging to one but not both of two sets.
 
+#(set (concat (remove %1 %2) (remove %2 %1)))
+
 (#(set (concat (apply disj %1 %2)
                (apply disj %2 %1)))
  #{1 2 3 4 5 6} #{1 3 5 7})
