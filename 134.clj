c4e1957ea6180bcea16e307687aef4d85575c92c
Bobby Towers <porkostomus@mail.com>
2018-05-23 09:27:47 -0700

    Create 134.clj

---
+++ 134.clj
@@ -0,0 +1,13 @@
+A nil key
+Difficulty:	Elementary
+Topics:	maps
+
+Write a function which, given a key and map, returns true iff the map contains an entry with that key and its value is nil.
+
+(true?  (__ :a {:a nil :b 2}))
+
+(false? (__ :b {:a nil :b 2}))
+
+(false? (__ :c {:a nil :b 2}))
+
+#(= nil (%2 % 1))
