1737ef5891cf3f5320b861db943f5de86e2d503e
Bobby Towers <porkostomus@mail.com>
2018-05-23 13:00:34 -0700

    Update 46.clj

--- 46.clj
+++ 46.clj
@@ -1,5 +1,10 @@
-;; 46 Flipping Out
-; "Write a higher-order function which flips the order of the arguments of an input function."
+{:_id 46 :title "Flipping out"
+ :tests ["(= 3 ((__ nth) 2 [1 2 3 4 5]))"
+         "(= true ((__ >) 7 8))"
+         "(= 4 ((__ quot) 2 8))"
+         "(= [1 2 3] ((__ take) [1 2 3 4 5] 3))"]
+ :description "Write a higher-order function which flips the order of the arguments of an input function."
+ :tags ["medium" "higher-order-functions"]}
 
 (fn [f] #(f %2 %))
 
